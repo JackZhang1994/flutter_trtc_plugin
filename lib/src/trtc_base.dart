538af5a02ba70371cf307ae912fc6028f2a328e1
陈磊的MacBook Pro <200892907@qq.com>
2020-05-13 10:46:46 +0800

    fix:解决dynamic->map问题

--- lib/src/trtc_base.dart
+++ lib/src/trtc_base.dart
@@ -327,8 +327,8 @@ class TrtcBase {
       case 'onNetworkQuality':
         if (_onNetworkQuality != null) {
           Map localQualityMap = args['localQuality'];
-          TrtcUserQuality localQuality = TrtcUserQuality(localQualityMap['userId'], int.parse(localQualityMap['quality'].toString()));
-          List<Map> remoteQualityList = args['remoteQuality'];
+          TrtcUserQuality localQuality = TrtcUserQuality(localQualityMap['userId'], localQualityMap['quality']);
+          List<dynamic> remoteQualityList = args['remoteQuality'];
           List<TrtcUserQuality> remoteQuality = remoteQualityList.map((value) {
             return TrtcUserQuality(value['userId'], value['quality']);
           }).toList();
